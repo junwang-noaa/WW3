526b02d71cea716e65afa20f93e42cd10352f1c5
breichl@my.uri.edu <breichl@my.uri.edu>
2013-06-28 16:56:38 +0000

    Merged trunk changes r27608:29554 into branch airsea_coupling (finishing r29555 which just updated directory bin)
    Former-commit-id: 6fe11c7765dc9b48ef3d962b3c1883be90eb5436

--- model/ftn/SCRIP/SCRIP.mk
+++ model/ftn/SCRIP/SCRIP.mk
@@ -14,16 +14,10 @@ $(aPo)/scrip_errormod.o: SCRIP/scrip_errormod.f90 \
 	$(aPo)/scrip_iounitsmod.o
 	@$(aPb)/ad3 scrip_errormod
 
-$(aPo)/scrip_netcdfmod.o: SCRIP/scrip_netcdfmod.f90 \
-	$(aPo)/scrip_kindsmod.o \
-	$(aPo)/scrip_errormod.o
-	@$(aPb)/ad3 scrip_netcdfmod
-
 $(aPo)/scrip_grids.o: SCRIP/scrip_grids.f \
 	$(aPo)/scrip_kindsmod.o \
 	$(aPo)/scrip_constants.o \
 	$(aPo)/scrip_iounitsmod.o \
-	$(aPo)/scrip_netcdfmod.o \
 	$(aPo)/scrip_errormod.o
 	@$(aPb)/ad3 scrip_grids
 
@@ -32,8 +26,7 @@ $(aPo)/scrip_remap_vars.o: SCRIP/scrip_remap_vars.f \
 	$(aPo)/scrip_constants.o \
 	$(aPo)/scrip_grids.o \
 	$(aPo)/scrip_errormod.o \
-	$(aPo)/scrip_iounitsmod.o \
-	$(aPo)/scrip_netcdfmod.o
+	$(aPo)/scrip_iounitsmod.o 
 	@$(aPb)/ad3 scrip_remap_vars
 
 $(aPo)/scrip_remap_conservative.o: SCRIP/scrip_remap_conservative.f \
@@ -43,34 +36,21 @@ $(aPo)/scrip_remap_conservative.o: SCRIP/scrip_remap_conservative.f \
 	$(aPo)/scrip_remap_vars.o \
 	$(aPo)/scrip_grids.o \
 	$(aPo)/scrip_errormod.o \
-	$(aPo)/scrip_iounitsmod.o \
-	$(aPo)/scrip_netcdfmod.o
+	$(aPo)/scrip_iounitsmod.o 
 	@$(aPb)/ad3 scrip_remap_conservative
 
 $(aPo)/scrip_timers.o: SCRIP/scrip_timers.f \
 	$(aPo)/scrip_kindsmod.o
 	@$(aPb)/ad3 scrip_timers
 
-$(aPo)/scrip_remap_write.o: SCRIP/scrip_remap_write.f \
-	$(aPo)/scrip_kindsmod.o \
-	$(aPo)/scrip_constants.o \
-	$(aPo)/scrip_netcdfmod.o \
-	$(aPo)/scrip_remap_vars.o \
-	$(aPo)/scrip_grids.o \
-	$(aPo)/scrip_errormod.o \
-	$(aPo)/scrip_iounitsmod.o
-	@$(aPb)/ad3 scrip_remap_write
-
-$(aPo)/scrip_interface.o: SCRIP/scrip_interface.f90 \
+$(aPo)/scrip_interface.o: SCRIP/scrip_interface.ftn \
 	$(aPo)/scrip_kindsmod.o \
 	$(aPo)/scrip_constants.o \
 	$(aPo)/scrip_timers.o \
 	$(aPo)/scrip_remap_vars.o \
 	$(aPo)/scrip_grids.o \
 	$(aPo)/scrip_remap_conservative.o \
-	$(aPo)/scrip_remap_write.o \
 	$(aPo)/scrip_iounitsmod.o \
-	$(aPo)/scrip_errormod.o \
-	$(aPo)/scrip_netcdfmod.o
+	$(aPo)/scrip_errormod.o 
 	@$(aPb)/ad3 scrip_interface
 
